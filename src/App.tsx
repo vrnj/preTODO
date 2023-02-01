0bc611a76419d6dc0957aa51f2e475b3df231376
ju-voro <voronetskajaj@hotmail.com>
2023-02-01 10:33:57 +0100

    add useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import { Todolist } from './components/Todolist';
 
@@ -9,18 +9,23 @@ function App() {
     const hat3 = 'Foo';
     const cuteLittleHat = 'My cute little hat';
 
-    let tasks1 = [
+    let initTasks = [
         { id: 1, title: "HTML&CSS", isDone: true },
         { id: 2, title: "JS", isDone: true },
         { id: 3, title: "ReactJS", isDone: false },
         { id: 4, title: "Redux", isDone: false }
     ];
 
+    //useState(initTasks); //is imported from React
+
+    let stateArray = useState(initTasks);
+    let tasks = stateArray[0]; //the copy of the initial array
+    let setTasks = stateArray[1]; //the function that is capable of managing the initial array after setTasks was called inside any other function changing the array
+
     function deleteTask(id: number) {
-        tasks1 = tasks1.filter(task => task.id !== id
-            // if(task.id !== id) return true; //adding the element to the updatedTasks array
-            // else return false //omitting the element
+            let filteredTasks = tasks.filter(task => task.id !== id
         );
+        setTasks(filteredTasks); //we are passing the filteredTasks array to the setTasks function, which re-write the tasks array at stateArray[0]
     }
 
     //this data (tasks1 object can only be managed by a function within the same location)
@@ -37,7 +42,7 @@ function App() {
             <Todolist 
                 hat={hat1} 
                 cuteHat={cuteLittleHat} 
-                tasks={tasks1}          
+                tasks={tasks}          
                 deleteTask={deleteTask}
             />
             {/* <Todolist hat={hat2} cuteHat={cuteLittleHat} tasks={tasks2}/> */}
